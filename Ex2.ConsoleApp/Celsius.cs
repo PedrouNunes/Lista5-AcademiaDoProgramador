e77279408f2b154736c882d8dd0c71658b79f3c4
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-08 16:08:33 -0300

    adicionado exercício 2

---
+++ Ex2.ConsoleApp/Celsius.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex2.ConsoleApp
+{
+    internal partial class Program
+    {
+        public class Celsius
+        {
+            public double fahrenheit;
+            public double celsius;
+
+            public void Conversao()
+            {
+                celsius = (fahrenheit - 32) / 1.8;
+            }
+        }
+    }
+    }
+
---
+++ Ex2.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex2.ConsoleApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius conversao = new Celsius();
+
+            conversao.fahrenheit = 86;
+
+            conversao.Conversao();
+
+            Console.WriteLine("Temperatura de graus Fahrenheit para Celcius: " + conversao.celsius + " C° ");
+        }
+    }
+}
