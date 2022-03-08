3f7dccdd50eb5b667b37d2170991305fd45dd09f
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-08 15:46:51 -0300

    Add project files.

---
+++ Ex1_Lista5.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex1_Lista5.ConsoleApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            caixaRetangular caixa = new caixaRetangular();
+
+            caixa.altura = 5;
+            caixa.largura = 6;
+            caixa.comprimento = 7;
+
+            caixa.Volume();
+            Console.WriteLine("O volume da caixa retângular é de: " + caixa.volumeCaixa + "m².");
+    }
+    }
+}
---
+++ Ex1_Lista5.ConsoleApp/caixaRetangular.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex1_Lista5.ConsoleApp
+{
+    internal partial class Program
+    {
+        public class caixaRetangular
+        {
+            public decimal altura;
+            public decimal comprimento;
+            public decimal largura;
+            public decimal volumeCaixa;
+
+            public void Volume()
+            {
+                volumeCaixa = altura * comprimento * largura;
+            }
+        }
+    }
+    }
+
