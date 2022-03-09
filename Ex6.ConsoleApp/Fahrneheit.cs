861869b373ae2d741d341d58baa70ffb7fb274fd
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-09 16:41:55 -0300

    ex 6 implementado

---
+++ Ex6.ConsoleApp/Fahrneheit.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex6.ConsoleApp
+{
+    internal partial class Program
+    {
+        public class Fahrneheit
+        {
+            public double fahrenheit;
+            public double celsius;
+
+            public double Conversao()
+            {
+                fahrenheit = celsius * 1.8 + 32;
+                return fahrenheit;
+            }
+        }
+    }
+    }
+
---
+++ Ex6.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex6.ConsoleApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Fahrneheit conversao = new Fahrneheit();
+
+            conversao.celsius = 30;
+
+            conversao.Conversao();
+
+            Console.WriteLine("Temperatura de graus Celcius para Fahrenheit: " + conversao.Conversao() + "F° ");
+    }
+    }
+}
