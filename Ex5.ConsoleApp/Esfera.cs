7b37b144450a326924a52fd1704029400b815f35
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-09 16:34:54 -0300

    ex 5 implementado

---
+++ Ex5.ConsoleApp/Esfera.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex5.ConsoleApp
+{
+    internal partial class Program
+    {
+        public class Esfera
+        {
+            public double diametro;
+            private double volume;
+            private double raio;
+
+            public double volumeEsfera(double pi)
+            {
+                raio = diametro / 2;
+                volume = ((4 * pi) * (raio * raio * raio))/3;
+                return volume;
+            }
+        }
+        }
+    }
+
---
+++ Ex5.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex5.ConsoleApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+          Esfera esfera = new Esfera();
+
+         double pi = 3.14;
+
+            esfera.diametro = 8;
+
+            Console.WriteLine("O volume da esfera é: " + esfera.volumeEsfera(pi) + "cm³.");
+        
+
+
+       
+        }
+    }
+}
