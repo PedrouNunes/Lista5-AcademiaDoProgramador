4dfc033d26006338e9030a0a2d9c5cdecb832733
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-09 16:20:13 -0300

    Ex 4 implementado

---
+++ Ex4.ConsoleApp/Consumo.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex4.ConsoleApp
+{
+    internal partial class Program
+    {
+        public class Consumo
+        {
+            public double kmInicial;
+            public double kmFinal;
+            public int consumoDeCombustivel;
+            public double consumoTotal;
+
+            public double consumoPorKm()
+            {
+                consumoTotal = (kmFinal - kmInicial) / consumoDeCombustivel;
+                return consumoTotal;
+            }
+        }
+        }
+    }
+
---
+++ Ex4.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex4.ConsoleApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Consumo consumo = new Consumo();
+
+            consumo.kmInicial = 200;
+            consumo.kmFinal = 500;
+            consumo.consumoDeCombustivel = 40;
+
+            Console.WriteLine("Consumo de combustível por km desse veículo é de: " + consumo.consumoPorKm() + "Km/L.");
+        }
+    }
+}
