5eda21e4a6f40bc95ec086afd7adeb67c48e2586
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-03-09 09:46:35 -0300

    Ex 3 implementado

---
+++ Ex3.ConsoleApp/Cilindro.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex3.ConsoleApp
+{
+    internal partial class Program
+    {
+        public class Cilindro
+        {
+           
+            public double diametro;
+            public double altura;
+            public double volumeDoCilindro;
+            private double areaDaBase;
+
+            public void areaBase(double pi)
+            {
+                areaDaBase = pi * ((diametro/2) * (diametro/2));
+            }
+
+            public void volumeCilindro()
+            {
+                volumeDoCilindro = areaDaBase * altura;
+            }
+        }
+    }
+    }
+
---
+++ Ex3.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex3.ConsoleApp
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+               double pi = 3.14;
+
+            Cilindro cilindro = new Cilindro();
+
+            cilindro.altura = 5;
+            cilindro.diametro = 6;
+
+            cilindro.areaBase(pi);
+            cilindro.volumeCilindro();
+
+            Console.WriteLine("O volume do cilindro é: " + cilindro.volumeDoCilindro + "L².");
+            
+    }
+    }
+}
